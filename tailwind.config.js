f79c083640785d9959b24ecd5284aa0192011cbe
Ahnaf <hossainalvi384@gmail.com>
2024-12-04 23:59:42 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      backgroundColor: {
+        headerBg: "#264653",
+        btnColor: "#E76F51",
+      },
+    },
+  },
+  plugins: [],
+};
